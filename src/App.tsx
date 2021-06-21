26c41fe76104616f298a26c8b7a98ef11d961e88
yaminim1 <32393337+yaminim1@users.noreply.github.com>
2021-06-21 09:03:03 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -127,18 +127,6 @@ function App() {
         } catch (err) {
             console.log("Fail");
         }
-       /* if (document.selection) {
-            var range = document.body.createTextRange();
-            range.moveToElementText(document.getElementById(containerid));
-            range.select().createTextRange();
-            document.execCommand("copy");
-          } else if (window.getSelection) {
-            var range = document.createRange();
-            range.selectNode(document.getElementById(containerid));
-            window.getSelection().addRange(range);
-            document.execCommand("copy");
-            alert("Text has been copied, now paste in the text-area")
-          }*/
     };
 
     const isStateChanged = () => {
